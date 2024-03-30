3c749e755c8af668c1bccb72cf290a6815e674cc
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-03-30 21:19:57 +0900

    firebase Project
    firebase@10.1.0 설치

---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF86ytWLeFxXQh0etsB9nH7giaEuasWnw",
+  authDomain: "nwitter-reloaded-ffd6e.firebaseapp.com",
+  projectId: "nwitter-reloaded-ffd6e",
+  storageBucket: "nwitter-reloaded-ffd6e.appspot.com",
+  messagingSenderId: "256591902473",
+  appId: "1:256591902473:web:dfffed1eed6dc3d32485d8"
+};
+
+const app = initializeApp(firebaseConfig);
