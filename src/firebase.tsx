4cd1385ac7b9d101563927092bb7eb201f04d883
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-03-31 13:17:54 +0900

    Setup(Authentication)
    Auth.authStateReady()
    - 초기 인증 상태가 해결되면 promise를 반환한다.
    - promise가 확인되면 현재 사용자는 유효한 사용자이거나, 사용자가 로그아웃 한 경우 null일 수 있다.
    
    Auth.currentUser
    - 현재 로그인한 사용자(또는 null)
    
    Auth.signOut()
    - 현자 사용자를 로그아웃한다.
    - 사용자의 ID 토큰을 자동으로 취소하지 않는다.
    
    Auth.onAuthStateChanged()
    - 사용자의 로그인 상태 변경에 대한 관찰자를 추가한다.

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDF86ytWLeFxXQh0etsB9nH7giaEuasWnw",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
