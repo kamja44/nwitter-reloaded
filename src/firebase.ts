7418e4a9fad9c6de38eaa21e1d2673a142bee0dc
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-04-05 22:30:27 +0900

    Tweeting to Firebase
    Collection
    - 컬렉션은 데이터를 담고 있는 문서의 집합이다.
    
    Document
    - 컬렉션에는 Cloud FireStore의 스토리지 단위인 document가 하나 이상 있어야 한다.
    - document는 데이터를 필드로 저장한다.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDF86ytWLeFxXQh0etsB9nH7giaEuasWnw",
@@ -7,9 +9,13 @@ const firebaseConfig = {
   projectId: "nwitter-reloaded-ffd6e",
   storageBucket: "nwitter-reloaded-ffd6e.appspot.com",
   messagingSenderId: "256591902473",
-  appId: "1:256591902473:web:dfffed1eed6dc3d32485d8"
+  appId: "1:256591902473:web:dfffed1eed6dc3d32485d8",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
