0c43badd56db53455523960e1fdafad673a8f650
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-04-05 22:11:13 +0900

    Post Tweet Form

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,12 @@
-import { auth } from "../firebase";
+import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
 
-export default function Home(){
-    const logOut = () => {
-        auth.signOut();
-    }
-    return <h1>
-            <button onClick={logOut}>Log Out</button>
-        </h1>;
+const Wrapper = styled.div``;
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
+  );
 }
